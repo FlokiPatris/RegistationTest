1231ae4904fa2590dea581a0afad0a2109192aa8
Floki Patris <f.kotras@gmail.com>
2025-06-24 20:06:56 +0200

    Run in headlessMode by default.

--- conftest.py
+++ conftest.py
@@ -4,11 +4,9 @@ import pytest
 @pytest.fixture(scope="session")
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        viewport: ViewportSize = {"width": 1920, "height": 1080}
-        context = browser.new_context(viewport=viewport)
+        browser = p.chromium.launch()
+        context = browser.new_context()
         page = context.new_page()
-        page.set_default_timeout(60000)
 
         yield page
 
